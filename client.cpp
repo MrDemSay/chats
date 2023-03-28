5a62515eb4ebdb9c302d8c52859526c6e82b1d3b
MrDemSay <demsayf@yandex.ru>
2023-03-28 22:43:12 +0400

    first version

---
+++ client.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_CLOSE_CONNECTION_SYMBOL '#'
+#define DEFAULT_PORT 1601
+#define ERROR_S "CLIENT ERROR: "
+#define BUFFER_SIZE 1024
+
+bool is_client_connection_close(const char* msg);
+
+int main(int args, char const* argv[]) {
+    int client;
+
+    struct sockaddr_in server_address;
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    if (client < 0) {
+        std::cout << ERROR_S << "establishing socket error." << std::endl;
+        exit(0);
+    }
+
+    server_address.sin_port = htons(DEFAULT_PORT);
+    server_address.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr);
+
+    std::cout << "Client socket was created!\n";
+
+    int ret = connect(client, reinterpret_cast<const struct sockaddr*>(&server_address), sizeof(server_address));
+    if(ret == 0) {
+        std::cout << "=> Connection to server " << inet_ntoa(server_address.sin_addr) 
+                  << "with port number: " << DEFAULT_PORT << std::endl;
+    }
+
+    char buffer[BUFFER_SIZE];
+    std::cout << "Waiting for server confirmation...\n";
+    recv(client, buffer, BUFFER_SIZE, 0);
+    std::cout << "=> Connection established.\n" << "Enter " 
+              << SERVER_CLOSE_CONNECTION_SYMBOL << " for close the connection.\n";
+
+    while(true) {
+        std::cout << "Client: ";
+        std::cin.getline(buffer, BUFFER_SIZE);
+        send(client, buffer, BUFFER_SIZE, 0);
+        if(is_client_connection_close(buffer)) {
+            break;
+        }
+        std::cout << "Server: ";
+        recv(client, buffer, BUFFER_SIZE, 0);
+        std::cout << buffer;
+        if(is_client_connection_close(buffer)) {
+            break;
+        }
+        std::cout << std::endl;
+    }
+    close(client);
+    std::cout << "GoodBye. Connection closed. \n";
+        
+
+    return 0;
+}
+
+bool is_client_connection_close(const char* msg) {
+    for(auto i = 0; i < strlen(msg); ++i) {
+        if(msg[i] == SERVER_CLOSE_CONNECTION_SYMBOL) {
+            return true;
+        }
+    }
+    return false;
+}
